288acca2715c2f0a4ca4622738af6197428e53a5
Athira <athira@creopedia.com>
2020-03-27 12:26:33 +0530

    Change error messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ConfirmEqualValidatorDirective } from './confirm-equal-validator.directive';
+
+declare var NgForm:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'athira-app';
+export class AppComponent implements OnInit {
+  title = 'frorm-validation';
+  regForm:FormGroup;
+  submitted = false;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.regForm = this.fb.group({
+      'name': ["", Validators.required],
+      'email': ["",  Validators.required, Validators.email],
+      'phone': ["", Validators.required],
+      'password': ["", Validators.required, Validators.minLength(6)],
+      'confirmPassword': ["", Validators.required],
+      'tnc': ["", Validators.required]
+    });
+  }
+
+  get fval() {
+    return this.regForm.controls;
+  }
+
+  signup() {
+    console.log(this.regForm.value);
+    this.submitted = true;
+    if (this.regForm.invalid == false) {
+      return;
+    }
+    alert('form fields are validated successfully!');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ConfirmEqualValidatorDirective } from './confirm-equal-validator.directive';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConfirmEqualValidatorDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
